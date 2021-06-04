9a459f9cab1a0b7ba993b83e7c62f47f840ea996
Eugenio <76439879+eugenio114@users.noreply.github.com>
2021-06-04 16:49:34 +0200

    Add files via upload

---
+++ turtle-squares.py
@@ -0,0 +1,36 @@
+
+import turtle
+
+fig = turtle.Turtle()
+
+#fig.forward(100)
+#fig.left(45)
+#fig.forward(100)
+#fig.right(90)
+#fig.forward(100)
+
+fig.color("blue", "cyan")
+
+#fig.begin_fill()
+fig.forward(100)
+fig.left(90)
+fig.forward(100)
+fig.left(90)
+fig.forward(100)
+fig.left(90)
+fig.forward(100)
+
+fig.penup()
+fig.forward(10)
+fig.pendown()
+
+fig.forward(100)
+fig.left(90)
+fig.forward(100)
+fig.left(90)
+fig.forward(100)
+fig.left(90)
+fig.forward(100)
+#fig.end_fill()
+
+turtle.done()
